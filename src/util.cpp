888be11b63ff5006be4e19653106b06a8b3f6bc8
Sam Kravitz <samkravitz99@gmail.com>
2022-01-31 17:17:26 -0500

    Add Util function to get algebraic notation of a square

--- src/util.cpp
+++ src/util.cpp
@@ -24,3 +24,20 @@ std::tuple <int, int> Util::rank_file_from_square(Square square)
     int file = square % 8;
     return std::make_tuple(rank, file); 
 }
+
+/**
+ * @brief gets the algebraic notation representation of a square
+ * @param square board-index of square to turn to algebraic notation
+ * @return the algebraic representation of the square
+ * 
+ * example invocation:
+ * auto algebraic = to_algebraic(E4) => "e4"
+ */
+std::string Util::to_algebraic(Square square)
+{
+    std::string res = "";
+    auto [rank, file] = rank_file_from_square(square);
+    res += file + 'a';
+    res += std::to_string(rank + 1);
+    return res;    
+}
