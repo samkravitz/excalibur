1ce24bd108d2f0247a2cfa1dd9c2179958a70054
Sam Kravitz <samkravitz99@gmail.com>
2022-01-31 09:47:58 -0500

    Add main function & Makefile

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/* excalibur
+ * License: GPLv2
+ * See LICENSE for full license text
+ * Author: Sam Kravitz
+ * 
+ * FILE: main.cpp
+ * DATE: January 31st, 2022
+ * DESCRIPTION: main()
+ */
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
