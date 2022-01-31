5732bd2b00db47fbf8de50a26931de5eb1cbfde6
Sam Kravitz <samkravitz99@gmail.com>
2022-01-31 12:32:06 -0500

    Add function to iterate over set bits of a bitboard

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -12,6 +12,8 @@
 
 #include "bitboard.h"
 
+#include <bit>
+
 Bitboard::Bitboard(u64 bitboard)
     : bitboard(bitboard)
 {
@@ -21,4 +23,26 @@ Bitboard::Bitboard(u64 bitboard)
 void Bitboard::set(u64 bitboard)
 {
     bitboard = bitboard;
+}
+
+/**
+ * @brief iterate over all set bits of a bitboard, applying a callback function to each set bit
+ * 
+ * @param callback function to apply to each set bit of a bitboard. The argument to the callback is
+ * the board-index (8 * rank + file) of each set bit.
+ *
+ * @return None
+ */
+void Bitboard::iterate(std::function<void(int)> callback) const
+{
+	// agorithm adopted from https://lemire.me/blog/2018/02/21/iterating-over-set-bits-quickly, 
+	// with some changes by me
+    u64 bitset = bitboard;
+    while (bitset)
+    {
+    	u64 t = bitset & -bitset;
+    	int idx = std::countr_zero(bitset); 
+		callback(idx);
+		bitset ^= t;
+    }
 }
