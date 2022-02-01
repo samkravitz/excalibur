52fc49c801c76a4bc984842f20b70d4de13b31aa
Sam Kravitz <samkravitz99@gmail.com>
2022-01-31 20:12:47 -0500

    commit

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+#include "bitset_iter.h"
 #include "constants.h"
 
 std::vector<Move> pseudolegal(Board const &board, Color c)
@@ -11,28 +12,40 @@ std::vector<Move> pseudolegal(Board const &board, Color c)
     // get all pieces of our color
     auto pieces = board.pieces(c);
 
-    auto pawns = board.pieces(PAWN, c);
-    pawns.iterate([&](int from)
-    {
-        auto moves_bb = Bitboard(Constants::pawn_push_table[c][from]);
-        moves_bb.iterate([&](int to)
-        {
-            moves.push_back(Move(from, to));
-        });
-    });
+    //auto pawns = board.pieces(PAWN, c);
+    //pawns.iterate([&](int from)
+    //{
+    //    auto moves_bb = Bitboard(Constants::pawn_push_table[c][from]);
+    //    moves_bb.iterate([&](int to)
+    //    {
+    //        moves.push_back(Move(from, to));
+    //    });
+    //});
 
     auto knights = board.pieces(KNIGHT, c);
-    knights.iterate([&](int from)
+
+    for (auto const &from : bitset::indices_on(std::bitset<64>{knights}))
     {
-        auto moves_bb = Bitboard(Constants::knight_move_table[from]);
-        moves_bb.iterate([&](int to)
+        auto moves_bb = Constants::knight_move_table[from];
+        for (auto const &to : bitset::indices_on(std::bitset<64>{moves_bb}))
         {
-            auto intersection = moves_bb & to;
-            // discount moves that we have a piece on that square
-            if (intersection.empty())
+            if ((pieces & to) == 0)
                 moves.push_back(Move(from, to));
-        });
-    });
+        }
+    }
+
+    
+    //knights.iterate([&](int from)
+    //{
+    //    auto moves_bb = Bitboard(Constants::knight_move_table[from]);
+    //    moves_bb.iterate([&](int to)
+    //    {
+    //        auto intersection = moves_bb & to;
+    //        // discount moves that we have a piece on that square
+    //        if (intersection.empty())
+    //            moves.push_back(Move(from, to));
+    //    });
+    //});
 
     return moves;
 }
