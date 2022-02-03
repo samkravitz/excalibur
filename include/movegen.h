121f939bef19320063220f27efbfad7658478281
Sam Kravitz <samkravitz99@gmail.com>
2022-02-03 00:07:34 -0500

    Generate pseudolegal moves of sliding pieces

--- include/movegen.h
+++ include/movegen.h
@@ -20,3 +20,14 @@ std::vector<Move> legal(Board const &, Color);
 
 inline std::vector<Move> pseudolegal(Board const &board) { return pseudolegal(board, board.mover()); }
 inline std::vector<Move> legal(Board const &board) { return legal(board, board.mover()); }
+
+template <Direction>
+u64 ray_attacks(Square, u64);
+
+template <PieceType>
+u64 sliding_attacks(Square, u64);
+
+u64 diagonal_attacks(Square, u64);
+u64 antidiagonal_attacks(Square, u64);
+u64 rank_attacks(Square, u64);
+u64 file_attacks(Square, u64);
--- src/movegen.cpp
+++ src/movegen.cpp
@@ -60,6 +60,63 @@ std::vector<Move> pseudolegal(Board const &board, Color c)
         }
     }
 
+    // get occupancy set (all pieces)
+    u64 occ = board.pieces();
+
+    // get pseudolegal rook moves
+    u64 rooks = board.pieces(ROOK, c);
+    while (rooks)
+    {
+        Square from = bitscan(rooks);
+        u64 attacks = sliding_attacks<ROOK>(from, occ);
+
+        // filter out attacked squares that are occupied by one of our pieces
+        attacks &= ~our_pieces;
+        while (attacks)
+        {
+            Square to = bitscan(attacks);
+            moves.push_back(Move(from, to));
+            attacks &= attacks - 1;
+        }
+        rooks &= rooks - 1;
+    }
+
+    // get pseudolegal bishop moves
+    u64 bishops = board.pieces(BISHOP, c);
+    while (bishops)
+    {
+        Square from = bitscan(bishops);
+        u64 attacks = sliding_attacks<BISHOP>(from, occ);
+
+        // filter out attacked squares that are occupied by one of our pieces
+        attacks &= ~our_pieces;
+        while (attacks)
+        {
+            Square to = bitscan(attacks);
+            moves.push_back(Move(from, to));
+            attacks &= attacks - 1;
+        }
+        bishops &= bishops - 1;
+    }
+
+    // get pseudolegal queen moves
+    u64 queens = board.pieces(QUEEN, c);
+    while (queens)
+    {
+        Square from = bitscan(queens);
+        u64 attacks = sliding_attacks<QUEEN>(from, occ);
+
+        // filter out attacked squares that are occupied by one of our pieces
+        attacks &= ~our_pieces;
+        while (attacks)
+        {
+            Square to = bitscan(attacks);
+            moves.push_back(Move(from, to));
+            attacks &= attacks - 1;
+        }
+        queens &= queens - 1;
+    }
+
     return moves;
 }
 
@@ -67,4 +124,63 @@ std::vector<Move> legal(Board const &board, Color c)
 {
     std::vector<Move> moves;
     return moves;
+}
+
+template <PieceType pt>
+u64 sliding_attacks(Square square, u64 occupied)
+{
+    static_assert(pt != PAWN && pt != KNIGHT && pt != KING, "can only use attacks() for sliding pieces!");
+
+    if constexpr (pt == BISHOP)
+        return diagonal_attacks(square, occupied) | antidiagonal_attacks(square, occupied);
+    else if constexpr(pt == ROOK)
+        return file_attacks(square, occupied) | rank_attacks(square, occupied);
+    else
+        return diagonal_attacks(square, occupied) | antidiagonal_attacks(square, occupied) | file_attacks(square, occupied) | rank_attacks(square, occupied);
+
+}
+
+template <Direction d>
+u64 ray_attacks(Square square, u64 occupied)
+{
+    u64 attacks = Constants::ray_attack_table[d][square];
+    
+    // blocker set - pieces that block the ray
+    u64 blockers = attacks & occupied;
+
+    if (blockers == 0)
+        return attacks;
+    
+    // get square of first blocker
+    Square first_blocker;
+    
+    // positive ray attacks - bitscan forward
+    if constexpr (d == EAST || d == NORTH || d == NORTHEAST || d == NORTHWEST)
+        first_blocker = bitscan<FORWARD>(blockers);
+    
+    // negative ray attacks - bitscan backwards
+    else if constexpr (d == WEST || d == SOUTH || d == SOUTHWEST || d == SOUTHEAST)
+        first_blocker = bitscan<REVERSE>(blockers);
+    
+    return attacks ^ Constants::ray_attack_table[d][first_blocker];
+}
+
+u64 diagonal_attacks(Square square, u64 occupied)
+{
+    return ray_attacks<NORTHEAST>(square, occupied) | ray_attacks<SOUTHWEST>(square, occupied);
+}
+
+u64 antidiagonal_attacks(Square square, u64 occupied)
+{
+    return ray_attacks<NORTHWEST>(square, occupied) | ray_attacks<SOUTHEAST>(square, occupied);
+}
+
+u64 rank_attacks(Square square, u64 occupied)
+{
+    return ray_attacks<EAST>(square, occupied) | ray_attacks<WEST>(square, occupied);
+}
+
+u64 file_attacks(Square square, u64 occupied)
+{
+    return ray_attacks<NORTH>(square, occupied) | ray_attacks<SOUTH>(square, occupied);
 }
