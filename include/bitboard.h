858005006c8a81239dfbc7eaca449cd661e280bb
Sam Kravitz <samkravitz99@gmail.com>
2022-02-03 00:29:19 -0500

    Add Bitboard namespace to hold bitboard constants

--- include/bitboard.h
+++ include/bitboard.h
@@ -83,3 +83,28 @@ inline u64 operator^(u64 bitboard, Square square) { return bitboard ^ (1ull << s
 inline u64 &operator&=(u64 &bitboard, Square square) { bitboard = bitboard & (1ull << square); return bitboard; }
 inline u64 &operator|=(u64 &bitboard, Square square) { bitboard = bitboard | (1ull << square); return bitboard; }
 inline u64 &operator^=(u64 &bitboard, Square square) { bitboard = bitboard ^ (1ull << square); return bitboard; }
+
+namespace Bitboard
+{
+    constexpr u64 RANK_BB[8] = {
+        0x00000000000000ff,
+        0x000000000000ff00,
+        0x0000000000ff0000,
+        0x00000000ff000000,
+        0x000000ff00000000,
+        0x0000ff0000000000,
+        0x00ff000000000000,
+        0xff00000000000000,
+    };
+
+    constexpr u64 FILE_BB[8] = {
+        0x0101010101010101, // A file
+        0x0202020202020202, // B file
+        0x0404040404040404, // C file
+        0x0808080808080808, // D file
+        0x1010101010101010, // E file
+        0x2020202020202020, // F file
+        0x4040404040404040, // G file
+        0x8080808080808080, // H file
+    };
+}
