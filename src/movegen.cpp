02c40ba127db4bccae3faf8f3a5094d00c7c6cd3
Sam Kravitz <samkravitz99@gmail.com>
2022-02-04 11:25:45 -0500

    Fixup non shifty with templates

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -47,7 +47,6 @@ std::vector<Move> pseudolegal(Board const &board)
         Square to = bitscan(single_pushes);
         Square from = static_cast<Square>(to + 8 * perspective);
         moves.push_back(Move(from, to));
-        single_pushes &= single_pushes - 1;
     }
 
     u64 double_pushes = double_push_targets<c>(pawns, ~occ);
@@ -56,32 +55,34 @@ std::vector<Move> pseudolegal(Board const &board)
         Square to = bitscan(double_pushes);
         Square from = static_cast<Square>(to + 16 * perspective);
         moves.push_back(Move(from, to));
-        double_pushes &= double_pushes - 1;
     }
 
-     generate pseudolegal knight moves
-    auto knights = board.pieces(KNIGHT, c);
-    for (const auto &from : indeces_set(knights))
+    // generate pseudolegal knight moves
+    u64 knights = board.pieces(KNIGHT, c);
+    while (knights)
     {
-        auto moves_bb = Constants::knight_move_table[from];
-        for (const auto &to : indeces_set(moves_bb))
+        Square from = bitscan(knights);
+        u64 moves_bb = Constants::knight_move_table[from];
+        // filter out attacked squares that are occupied by one of our pieces
+        moves_bb &= ~our_pieces;
+        while (moves_bb)
         {
-            if (!(our_pieces & static_cast<Square>(to)))
-                moves.push_back(Move(from, to));
+            Square to = bitscan(moves_bb);
+            moves.push_back(Move(from, to));
         }
     }
 
-    //// generate pseudolegal king moves
-    //auto king = board.pieces(KING, c);
-    //for (const auto &from : indeces_set(king))
-    //{
-    //    auto moves_bb = Constants::king_move_table[from];
-    //    for (const auto &to : indeces_set(moves_bb))
-    //    {
-    //        if (!(our_pieces & static_cast<Square>(to)))
-    //            moves.push_back(Move(from, to));
-    //    }
-    //}
+    // generate pseudolegal king moves
+    u64 king = board.pieces(KING, c);
+    Square from = bitscan(king);
+    u64 moves_bb = Constants::king_move_table[from];
+    // filter out attacked squares that are occupied by one of our pieces
+    moves_bb &= ~our_pieces;
+    while (moves_bb)
+    {
+        Square to = bitscan(moves_bb);
+        moves.push_back(Move(from, to));
+    }
 
     // get pseudolegal rook moves
     u64 rooks = board.pieces(ROOK, c);
@@ -96,9 +97,7 @@ std::vector<Move> pseudolegal(Board const &board)
         {
             Square to = bitscan(attacks);
             moves.push_back(Move(from, to));
-            attacks &= attacks - 1;
         }
-        rooks &= rooks - 1;
     }
 
     // get pseudolegal bishop moves
@@ -114,9 +113,7 @@ std::vector<Move> pseudolegal(Board const &board)
         {
             Square to = bitscan(attacks);
             moves.push_back(Move(from, to));
-            attacks &= attacks - 1;
         }
-        bishops &= bishops - 1;
     }
 
     // get pseudolegal queen moves
@@ -132,9 +129,7 @@ std::vector<Move> pseudolegal(Board const &board)
         {
             Square to = bitscan(attacks);
             moves.push_back(Move(from, to));
-            attacks &= attacks - 1;
         }
-        queens &= queens - 1;
     }
 
     return moves;
