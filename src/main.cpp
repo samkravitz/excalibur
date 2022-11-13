7ef0acd49c36829e0bdca917e10cdadfc27d0ed4
Sam Kravitz <samkravitz99@gmail.com>
2022-11-13 13:54:24 -0500

    Parse new cli arguments

--- src/main.cpp
+++ src/main.cpp
@@ -26,10 +26,24 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
-	for (int i = 1; i < argc; ++i)
-		parse_uci_move(std::string(argv[i]));
+	// time left in the game, total game time in milliseconds
+	int time_left, game_time;
 
-	const auto [move, eval] = search(5);
+	for (int i = 1; i < argc; ++i)
+	{
+		auto arg = std::string(argv[i]);
+
+		if (arg == "-g")
+			game_time = atoi(argv[++i]);
+
+		else if (arg == "-t")
+			time_left = atoi(argv[++i]);
+
+		else
+			parse_uci_move(std::string(arg));
+	}
+
+	const auto [move, eval] = search_time(game_time, time_left);
 
 	std::cout << move << "\n";
 	return 0;
